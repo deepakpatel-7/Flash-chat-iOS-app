496f7b619acb5cc2b241426a48d6e67278d412e2
deepakpatel-7 <deepakpatel6957@gmail.com>
2024-03-23 21:32:49 +0530

    added some improvement in the UI of the app

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -34,13 +34,12 @@ class ChatViewController: UIViewController {
     
     func loadMessages(){
        
-        
         db.collection(K.FStore.collectionName)
             .order(by: K.FStore.dateField)
             .addSnapshotListener{ (querySnapshot, error) in
             self.messages = []
             if let e = error {
-                print("There was an issue fro reteriving the data from the firestore, \(e)")
+                print("There was an issue from reteriving the data from the firestore, \(e)")
             }
             else{
                 if let snapshotDocuments = querySnapshot?.documents{
@@ -53,6 +52,8 @@ class ChatViewController: UIViewController {
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count-1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
@@ -68,7 +69,12 @@ class ChatViewController: UIViewController {
                 if let e = error {
                     print("There was an issuesaving data to the firestore, \(e)")
                           } else{
-                        print("Sucessfully saved data")
+                            print("Sucessfully saved data")
+                              
+                              DispatchQueue.main.async {
+                                  self.messageTextfield.text = ""
+                              }
+                              
                     }
                           
             }
@@ -93,9 +99,31 @@ extension ChatViewController : UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        as! MessageCell
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+//        cell.label.text = message.body
         cell.label.text = messages[indexPath.row].body
+//        print(messages[indexPath.row].body)
+//        print(cell.label.text!)
+//        print(message.sender)
+//        print(Auth.auth().currentUser?.email!)
+        
+        
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageVIew.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageVIew.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            
+        }
+       
         return cell
     }
 }
